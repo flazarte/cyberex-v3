136f3449cc24117648b4a41ae1a7211f60edd72e
flazarte <ferdinandlazarte2017@gmail.com>
2024-12-02 12:20:11 +0800

    VPN Revision

--- CTFd/plugins/custom/vpn/api.py
+++ CTFd/plugins/custom/vpn/api.py
@@ -1,186 +1,112 @@
-
 import json
-import os
-import requests, urllib3
+import urllib3
+import requests
 from flask import abort
-from pprint import pp, pprint #for Debugging purpose only remove in Production
-
-#Integration of Cyberex SecT-eX VPN Automation
-#VPN Username Super User
-CYBEREX_VPN_USERNAME = "Administrator"
-CYBEREX_VPN_PASSWORD = "Cyber@1992"
-#CYBEREX_VPN_URL = "https://121.58.248.18/api/"
-CYBEREX_VPN_URL = "https://121.58.248.26:5555/api/"
-CYBEREX_VPN_HUBNAME = "Cyberex-VPN"
-CYBEREX_VPN_GROUP_NAME = "Cyberex-Participants"
 
 class VPNConnector:
     """
-    A class to connect to a Cyberex VPN server and retrieve server information.
+    A class to manage Cyberex VPN operations.
     Author: PFC Ferdinand P Lazarte (Inf) PA
     DATE: March 2024
     """
-    
+
     def __init__(self):
         """
-        Initializes the VPNConnector class.
+        Initializes the VPNConnector with necessary configuration.
+        """
+        self.headers = {
+            "content-type": "application/json",
+            "X-VPNADMIN-HUBNAME": "Administrator",
+            "X-VPNADMIN-PASSWORD": "Cyber@1992",
+        }
+        self.gateway = "https://157.20.8.39:5555/api"
+        self.hub_name = "Cyberex-VPN"
+        self.group_name = "Cyberex-Participants"
+
+    def _send_request(self, method, params):
+        """
+        Sends a request to the VPN server.
 
         Args:
-            username (str): The username for authentication.
-            password (str): The password for authentication.
-            gateway (str): The URL of the VPN server.
+            method (str): The RPC method to call.
+            params (dict): The parameters for the RPC method.
+
+        Returns:
+            dict: The server's response as a dictionary.
+
+        Raises:
+            Exception: If the request fails or returns a non-200 status code.
         """
-
-  
-        self.headers = {'content-type': 'application/json',
-                            'X-VPNADMIN-HUBNAME': CYBEREX_VPN_USERNAME,
-                            'X-VPNADMIN-PASSWORD': CYBEREX_VPN_PASSWORD,
-                        }
-        self.gateway = CYBEREX_VPN_URL
-        self.hubname = CYBEREX_VPN_HUBNAME
-        self.groupname = CYBEREX_VPN_GROUP_NAME
-    
-    #get Cyberex VPN Server Info
-    def GetServerInfo(self):
-
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         payload = {
             "jsonrpc": "2.0",
             "id": "rpc_call_id",
-            "method": "Test",
-            "params": {
-                "IntValue_u32": 0
-            }
+            "method": method,
+            "params": params,
         }
-        
-        # disable warnings
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        #submit response to vpn server
-        response = requests.request("POST", url=self.gateway, headers=self.headers, data = json.dumps(payload),  verify=False)
-        #pprint({"status_code":response.status_code})
+        response = requests.post(
+            url=self.gateway, headers=self.headers, data=json.dumps(payload), verify=False
+        )
         if response.status_code == 200:
             return response.json()
         else:
-            raise Exception(f"Request failed with status code: {response.status_code}")
-        
+            raise Exception(
+                f"{method} failed with status code {response.status_code}: {response.text}"
+            )
 
-    #get Get Server Status
-    def GetServerStatus(self):
+    def get_server_info(self):
+        """
+        Retrieves information about the VPN server.
 
-        payload = {
-                    "jsonrpc": "2.0",
-                    "id": "rpc_call_id",
-                    "method": "GetServerStatus",
-                    "params": {}
-                }
-        
-        # disable warnings
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        #submit response to vpn server
-        response = requests.request("POST", url=self.gateway, headers=self.headers, data = json.dumps(payload),  verify=False)
-        #pprint({"status_code":response.status_code})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return abort(500, "Cyberex VPN Connection Error!")
-            #raise Exception(f"Request failed with status code: {response.status_code}")
-        
-    #get Create Cyberex VPN User based on their created user credentials
-    def CreateUser(self, username, user_full_name, user_password):
-        false = False
-        payload = {
-                    "jsonrpc": "2.0",
-                    "id": "rpc_call_id",
-                    "method": "CreateUser",
-                    "params": {
-                        "HubName_str": self.hubname,
-                        "Name_str": username,
-                        "Realname_utf": user_full_name,
-                        "GroupName_str": self.groupname,
-                        "Note_utf": "",
-                        "ExpireTime_dt": "",
-                        "AuthType_u32": 1,
-                        "Auth_Password_str": user_password,
-                        "UserX_bin": "",
-                        "Serial_bin": "",
-                        "CommonName_utf": "auth_rootcert_commonname",
-                        "RadiusUsername_utf": "auth_radius_radiususername",
-                        "NtUsername_utf": "auth_nt_ntusername",
-                        "UsePolicy_bool": false,
-                        "policy:Access_bool": false,
-                        "policy:DHCPFilter_bool": false,
-                        "policy:DHCPNoServer_bool": false,
-                        "policy:DHCPForce_bool": false,
-                        "policy:NoBridge_bool": false,
-                        "policy:NoRouting_bool": false,
-                        "policy:CheckMac_bool": false,
-                        "policy:CheckIP_bool": false,
-                        "policy:ArpDhcpOnly_bool": false,
-                        "policy:PrivacyFilter_bool": false,
-                        "policy:NoServer_bool": false,
-                        "policy:NoBroadcastLimiter_bool": false,
-                        "policy:MonitorPort_bool": false,
-                        "policy:MaxConnection_u32": 0,
-                        "policy:TimeOut_u32": 0,
-                        "policy:MaxMac_u32": 0,
-                        "policy:MaxIP_u32": 0,
-                        "policy:MaxUpload_u32": 0,
-                        "policy:MaxDownload_u32": 0,
-                        "policy:FixPassword_bool": false,
-                        "policy:MultiLogins_u32": 0,
-                        "policy:NoQoS_bool": false,
-                        "policy:RSandRAFilter_bool": false,
-                        "policy:RAFilter_bool": false,
-                        "policy:DHCPv6Filter_bool": false,
-                        "policy:DHCPv6NoServer_bool": false,
-                        "policy:NoRoutingV6_bool": false,
-                        "policy:CheckIPv6_bool": false,
-                        "policy:NoServerV6_bool": false,
-                        "policy:MaxIPv6_u32": 0,
-                        "policy:NoSavePassword_bool": false,
-                        "policy:AutoDisconnect_u32": 0,
-                        "policy:FilterIPv4_bool": false,
-                        "policy:FilterIPv6_bool": false,
-                        "policy:FilterNonIP_bool": false,
-                        "policy:NoIPv6DefaultRouterInRA_bool": false,
-                        "policy:NoIPv6DefaultRouterInRAWhenIPv6_bool": false,
-                        "policy:VLanId_u32": 0,
-                        "policy:Ver3_bool": false
-                    }
-                }
-        
-        # disable warnings
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        #submit response to vpn server
-        response = requests.request("POST", url=self.gateway, headers=self.headers, data = json.dumps(payload),  verify=False)
-        #pprint({"status_code":response.status_code})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return abort(500, "Cyberex VPN Connection Error!")
-            #raise Exception(f"Request failed with status code: {response.status_code}")
-        
-    #get Delete User
-    def DeleteUser(self, username):
+        Returns:
+            dict: Server information.
+        """
+        return self._send_request("Test", {"IntValue_u32": 0})
 
-        payload = {
-                    "jsonrpc": "2.0",
-                    "id": "rpc_call_id",
-                    "method": "DeleteUser",
-                    "params": {
-                        "HubName_str": self.hubname,
-                        "Name_str": username
-                    }
-                }
+    def get_server_status(self):
+        """
+        Retrieves the VPN server's current status.
 
-        
-        # disable warnings
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        #submit response to vpn server
-        response = requests.request("POST", url=self.gateway, headers=self.headers, data = json.dumps(payload),  verify=False)
-        #pprint({"status_code":response.status_code})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return abort(500, "Cyberex VPN Connection Error!")
-            #raise Exception(f"Request failed with status code: {response.status_code}")
-        
+        Returns:
+            dict: Server status.
+        """
+        return self._send_request("GetServerStatus", {})
+
+    def create_user(self, username, user_full_name, user_password):
+        """
+        Creates a new VPN user.
+
+        Args:
+            username (str): The username for the new user.
+            user_full_name (str): The full name of the user.
+            user_password (str): The password for the user.
+
+        Returns:
+            dict: The server's response to the user creation request.
+        """
+        params = {
+            "HubName_str": self.hub_name,
+            "Name_str": username,
+            "Realname_utf": user_full_name,
+            "GroupName_str": self.group_name,
+            "AuthType_u32": 1,
+            "Auth_Password_str": user_password,
+            "UsePolicy_bool": False,
+        }
+        return self._send_request("CreateUser", params)
+
+    def delete_user(self, username):
+        """
+        Deletes a VPN user.
+
+        Args:
+            username (str): The username of the user to delete.
+
+        Returns:
+            dict: The server's response to the deletion request.
+        """
+        params = {
+            "HubName_str": self.hub_name,
+            "Name_str": username,
+        }
+        return self._send_request("DeleteUser", params)
